0a36eb8f36b1b514c8ce15fe7a0f4c9ab8cce1ef
nileshpanalink <nilesh@panalinks.com>
2025-06-12 11:41:07 +0530

    error fix fix 2

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,10 +1,11 @@
+import HeaderJobs from '@/components/HeaderJobs'
 import React from 'react'
 
 export default function JobsApp() {
   return (
-    <div  className="min-h-screen bg-base-100 text-base-content font-sans transition-colors duration-300">
+    <div data-theme="night" className="min-h-screen bg-base-100 text-base-content font-sans transition-colors duration-300">
     
-      
+      <HeaderJobs />
     </div>
   )
 }
