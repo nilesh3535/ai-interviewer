b3e54cb07b92b57320f341482bee06d4fbfd7afe
nileshpanalink <nilesh@panalinks.com>
2025-06-12 13:50:56 +0530

    error fix fux 22

--- app/jobs/layout.tsx
+++ app/jobs/layout.tsx
@@ -1,6 +1,10 @@
+import { isAuthenticated } from '@/lib/actions/auth.action';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({ children }: { children: React.ReactNode }) {
+    const isUserAuthenticated = await isAuthenticated();
+        if (!isUserAuthenticated) redirect("/sign-in");
     return (
         <section>
             {children}
