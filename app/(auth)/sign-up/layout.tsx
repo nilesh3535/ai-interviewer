0581798e2f520f67a0203b0846cc4944fdb7f932
nileshpanalink <nilesh@panalinks.com>
2026-04-08 09:37:36 +0530

    layout files added for signin and signup

---
+++ app/(auth)/sign-up/layout.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+
+import { isAuthenticated } from "@/lib/actions/auth.action";
+
+const AuthLayout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (true) redirect("/construction");
+
+  return (
+    <div className="auth-layout">
+      {/* Top Left Logo */}
+      <div className="sm:absolute top-5 left-6">
+        <Link href="https://winyourinterview.ai/">
+          <Image
+            src="/wyi.png" // replace with your logo path
+            alt="Company Logo"
+            width={200}
+            height={100}
+            className="object-contain bg-gray-50 rounded-sm"
+          />
+        </Link>
+      </div>
+
+      {/* Centered Form */}
+      <div className="flex flex-col items-center justify-center w-full"></div>
+    </div>
+  );
+};
+
+export default AuthLayout;
